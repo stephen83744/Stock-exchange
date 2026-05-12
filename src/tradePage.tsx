1a293cb373ef4e40248517ef59d1cf2f2fc86f31
Akinyemi Stephen <149060446+stephen83744@users.noreply.github.com>
2026-05-12 01:49:22 +0100

    Update tradePage.tsx

--- src/tradePage.tsx
+++ src/tradePage.tsx
@@ -5,47 +5,67 @@ const TradePage: React.FC = () => {
   const [amount, setAmount] = useState("");
   const navigate = useNavigate();
   return (
-
-
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-       <header className="fixed top-0 z-50 flex h-14 w-full items-center justify-between  bg-gray-700  px-4 ">
-      <div className="flex items-center gap-3">
+           <header className="fixed top-0 z-50 flex h-14 w-full  items-center justify-between  bg-gray-700  px-4 ">
+        <div className="flex items-center gap-3">
+          <h1 className="text-lg font-extrabold text-white ">AlphaRise</h1>
+        </div>
 
-        <h1 className="text-lg font-extrabold text-white ">
-          AlphaRise
-        </h1>
-      </div>
+        <div className="flex items-center gap-6">
+          <nav className="hidden md:flex items-center gap-6">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/marketPage");
+              }}
+              className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400"
+            >
+              Market
+            </a>
 
-      <div className="flex items-center gap-6">
-        <nav className="hidden md:flex items-center gap-6">
-          <a href="#"
-            className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400" >
-            Market
-          </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/trends");
+              }}
+              className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400"
+            >
+              Trends
+            </a>
 
-          <a href="#"
-            className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400">
-            Trends
-          </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/market");
+              }}
+              className=" px-2 py-1 text-sm font-semibold text-slate-500 dark:text-white"
+            >
+              News
+            </a>
 
-          <a href="#" onClick={(e) => {e.preventDefault(); navigate("/market"); }} 
-            className=" px-2 py-1 text-sm font-semibold text-slate-500 dark:text-white">
-            News
-          </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/trade");
+              }}
+              className="border-b-2 border-primary px-2 py-1 text-sm font-bold text-slate-900 dark:text-slate-400"
+            >
+              Trade
+            </a>
+          </nav>
 
-          <a href="#" onClick={(e) => { e.preventDefault(); navigate("/trade"); }} className="border-b-2 border-primary px-2 py-1 text-sm font-bold text-slate-900 dark:text-slate-400"
-          >
-            Trade
-          </a>
-        </nav>
-
-        <button className="p-2">
-          <span className=" text-slate-900 text-white">
-            search
-          </span>
-        </button>
-      </div>
-    </header>
+          <button className="p-2" onClick={(e) => {
+            e.preventDefault();
+            navigate("/marketPlace");
+          }}>
+            <span className=" text-slate-900 text-white">search</span>
+          </button>
+        </div>
+      </header>
       <div className=" w-full w-lg rounded-2xl p-8 shadow-xl bg-white">
 
       <div className="flex bg-gray-100 p-1 rounded-lg mb-8">
@@ -53,8 +73,8 @@ const TradePage: React.FC = () => {
           Buy
         </div>
         <button
-          onClick={() => navigate("")}
-          className="flex-1 py-2 text-center text-sm font-medium text-gray-500 hover:text-gray-700"
+          onClick={() => navigate("/sell")}
+          className="flex-1 py-2 text-center text-sm font-medium text-gray-500 hover:text-gray-700 cursor-pointer"
         >
           Sell
         </button>
