e795fb003628b2dd1a67a1183c2a8b5ae1f2d426
Akinyemi Stephen <149060446+stephen83744@users.noreply.github.com>
2026-05-12 01:44:54 +0100

    Add new routes for Sell, Trends, and Market pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,21 @@ import MarketNewsPage from "./marketNewsPage.tsx";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import TradePage from "./tradePage.tsx";
-
+import SellPage from "./sellPage.tsx";
+import  TrendsPage from "./trendingStocks.tsx";
+import MarketPage from "./MarketPage.tsx";
+  
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/loginPage" element={<LoginPage />} />
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path ="/market" element={<MarketNewsPage />} />
         <Route path="/trade" element={<TradePage />} />
+        <Route path="/sell" element={<SellPage />} />
+        <Route path="/trends" element={<TrendsPage />} />
+        <Route path="/marketPage" element={<MarketPage/>}/>
       </Routes>
     </BrowserRouter>
   );
