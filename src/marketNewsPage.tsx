088f89a4ed9f85cd100fee2677660ba2ac596719
Akinyemi Stephen <149060446+stephen83744@users.noreply.github.com>
2026-05-12 01:48:45 +0100

    Refactor MarketNewsPage component structure

--- src/marketNewsPage.tsx
+++ src/marketNewsPage.tsx
@@ -1,36 +1,13 @@
 import { useState, useEffect } from "react";
-import { generateMarketNews } from "../api/apiCalls.js";
+import { generateMarketNews } from "../api/apiCalls.tsx";
 import { useNavigate } from "react-router-dom";
-import{searchtab} from "../api/apiCalls.js"
+
 
 export const MarketNewsPage: React.FC = () => {
     const navigate = useNavigate();
   const [markets, setMarkets] = useState<any[]>([]);
-  const [results, setResults] = useState<any[]>([]);
-    const [searchText, setSearchText] = useState("");
-    const [popup, setPopup] = useState(false);
 
-  const search =()=>{
-    setPopup(true);
-  };
-    
-  const searching =async ()=>{
-     if (!searchText.trim()) {
-    setResults([]);
-    return;
-  }
-
-  const result= await searchtab(searchText);
-
-  const allResults = [
-    ...result.data.stock,
-    ...result.data.index,
-    ...result.data.currency,
-  ];
-
-  setResults(allResults);
   
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -43,77 +20,67 @@ export const MarketNewsPage: React.FC = () => {
 
   
   return (
-    
-    
     <div className="min-h-screen bg-slate-50 bg-gray-300  py-8">
-        {popup && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black/20 backdrop-blur-md z-[99999] rounded-2xl">
-            <div className="bg-[#12141c] text-[#c5c6d9] p-5 rounded-lg shadow-2xl w-1/2 h-1/2 flex flex-col justify-center text-center">
-             <h1 className="text-2xl font-bold mb-4">Search Results</h1>
-              <input
-              className="w-full p-3 mb-3 rounded-md bg-[#1a1d27] text-white outline-none"
-              type="text"
-              value={searchText}
-                placeholder="Search Stocks, Indices, or Currencies"
-                onChange={(e) =>setSearchText(e.target.value)}
-            />
-                 {results.length > 0 && (
-                <div>
-               {results.map((item, index) => (
-                 <div key={index} >
-                {item.name
-      ? item.name
-      : item.from_symbol && item.to_symbol
-      ? `${item.from_symbol} → ${item.to_symbol}`
-      : "Unknown result"}
-                </div>
-                ))}
+   <header className="fixed top-0 z-50 flex h-14 w-full  items-center justify-between  bg-gray-700  px-4 ">
+        <div className="flex items-center gap-3">
+          <h1 className="text-lg font-extrabold text-white ">AlphaRise</h1>
         </div>
-      )}
-              <button onClick={searching} className="w-full p-3 bg-black rounded-md text-white cursor-pointer">
-               Search
-              </button>
-            </div>
-          </div>
-        )}
-            <header className="fixed top-0 z-50 flex h-14 w-full items-center justify-between  bg-gray-700  px-4 ">
-      <div className="flex items-center gap-3">
 
-        <h1 className="text-lg font-extrabold text-white ">
-          AlphaRise
-        </h1>
-      </div>
+        <div className="flex items-center gap-6">
+          <nav className="hidden md:flex items-center gap-6">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/marketPage");
+              }}
+              className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400"
+            >
+              Market
+            </a>
 
-      <div className="flex items-center gap-6">
-        <nav className="hidden md:flex items-center gap-6">
-          <a href="#"
-            className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400" >
-            Market
-          </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/trends");
+              }}
+              className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400"
+            >
+              Trends
+            </a>
 
-          <a href="#"
-            className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400">
-            Trends
-          </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/market");
+              }}
+              className=" px-2 py-1 text-sm font-bold text-slate-900 dark:text-white border-b-2 border-primary" 
+            >
+              News
+            </a>
 
-          <a href="#"
-            className="border-b-2 border-primary px-2 py-1 text-sm font-bold text-slate-900 dark:text-white">
-            News
-          </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/trade");
+              }}
+              className=" px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400"
+            >
+              Trade
+            </a>
+          </nav>
 
-          <a href="#" onClick={(e) => { e.preventDefault(); navigate("/trade"); }} className="px-2 py-1 text-sm font-semibold text-slate-500 dark:text-slate-400"
-          >
-            Trade
-          </a>
-        </nav>
-
-        <button className="p-2" onClick={search}>
-          <span className=" text-slate-900 text-white">
-            search
-          </span>
-        </button>
-      </div>
-    </header>
+          <button className="p-2" onClick={(e) => {
+            e.preventDefault();
+            navigate("/marketPlace");
+          }}>
+            <span className=" text-slate-900 text-white">search</span>
+          </button>
+        </div>
+      </header>
       <div className="max-w-7xl mx-auto mt-10">
         <h1 className="text-2xl font-bold text-slate-900 mb-6">
           Market News
@@ -121,7 +88,6 @@ export const MarketNewsPage: React.FC = () => {
 
         {/* Top hero section */}
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-          {/* Big left news */}
           <div className="lg:col-span-8">
             {markets[0] && (
               <div
